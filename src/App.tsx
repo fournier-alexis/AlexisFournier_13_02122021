7c2eec1bb76d0263d0e0c8a07951f59fbd84fc74
Alexis Fournier <pro.fournieralexis@gmail.com>
2021-12-17 10:34:59 +0100

    create global app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Footer } from "./components/modules/footer/footer";
+import { SignIn } from "./components/views/sign-in/signin";
+import { User } from "./components/views/user/user";
+import { Welcome } from "./components/views/welcome/welcome";
+import "./css/global.css";
+import { setUser } from "./features/user/userSlice";
+import { loadFromSessionStorage } from "./store";
 
 function App() {
+  const dispatch = useDispatch();
+  const storage = loadFromSessionStorage();
+
+  if (storage) {
+    dispatch(setUser(storage.user));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />}></Route>
+          <Route path="/sign-in" element={<SignIn />}></Route>
+          <Route path="/user" element={<User />}></Route>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
